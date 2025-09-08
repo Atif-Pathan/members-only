2d38869d5c3298e811e9c3bcc0c7b4ab8e18fd30
Atif Pathan <atifkhan.p1@gmail.com>
2025-09-07 20:53:27 -0600

    Update passport.js config to use the queries helper and use email as the username when authenticating

--- config/passport.js
+++ config/passport.js
@@ -4,36 +4,44 @@ const bcrypt = require('bcryptjs');
 const userQueries = require('../queries/userQueries');
 
 passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-      const user = rows[0];
+  new LocalStrategy(
+    { usernameField: 'email' }, // Tell Passport to use the 'email' field from the form
+    async (email, password, done) => {
+      try {
+        // Use your query function to find the user
+        const user = await userQueries.findByEmail(email);
 
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
+        // If no user is found
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email/user.' });
+        }
+
+        // Compare the submitted password with the hashed password in the DB
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          // Passwords do not match
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        // If everything is correct, return the user object
+        return done(null, user);
+      } catch (err) {
+        return done(err);
       }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        // passwords do not match!
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  }),
+    },
+  ),
 );
 
+// Stores the user's ID in the session cookie
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+// Retrieves the full user object from the DB based on the ID in the session
 passport.deserializeUser(async (id, done) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    const user = rows[0] || false;
-
-    done(null, user);
+    const user = await userQueries.findById(id);
+    done(null, user || false); // Use `false` if user is not found
   } catch (err) {
     done(err);
   }
