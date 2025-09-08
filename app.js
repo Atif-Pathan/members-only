c3b5375c1591f1855a447657528043a364fe5ed8
Atif Pathan <atifkhan.p1@gmail.com>
2025-09-07 20:55:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 require('dotenv').config();
-
-const path = require('node:path');
 const express = require('express');
+const path = require('path');
+const session = require('express-session');
 
 // Import configurations
 const sessionConfig = require('./config/session');
-const passport = require('./config/passport');
+require('./config/passport');
+const passport = require('passport');
 
 // Import middleware
 const { requestLogger } = require('./middleware/logging');
 const { attachUser } = require('./middleware/authRelated');
 const { notFound, errorHandler } = require('./middleware/errorHandler');
+const { queryMessages } = require('./middleware/queryMessages');
 
 // Import routes
-// const indexRouter = require('./src/routes/index');
+const indexRouter = require('./routes/indexRoutes');
 const authRouter = require('./routes/authRoutes');
-// const messageRouter = require('./src/routes/messages');
 
 const app = express();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public'))); // Static files
 app.use(express.urlencoded({ extended: false }));
 
+// session config
+app.use(session(sessionConfig));
+
+// Passport config
+app.use(passport.session());
+
+// Custom middlewares
+app.use(requestLogger);
+app.use(queryMessages);
+app.use(attachUser);
+
 // Routes
+app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
-// session
-
-app.get('/', (req, res, next) => {
-  res.send('<h1>Hello World</h1>');
-});
+// Error handling
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
